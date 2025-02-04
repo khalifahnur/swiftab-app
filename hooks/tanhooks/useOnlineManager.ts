740c6e8a03c57ea317c09adf33b989c392c1aea5
khalifahnur <khalifahnur1095@gmail.com>
2025-02-04 09:47:34 +0300

    tan-stack hooks

---
+++ hooks/tanhooks/useOnlineManager.ts
@@ -0,0 +1,9 @@
+import NetInfo from '@react-native-community/netinfo';
+import { onlineManager } from '@tanstack/react-query';
+
+export const useOnlineManager = onlineManager.setEventListener((setOnline) => {
+  return NetInfo.addEventListener((state) => {
+    setOnline(!!state.isConnected);
+    console.log(!!state)
+  });
+});
