d2dcc045015db5fc6b46f10aab200cc8e12a3c77
khalifahnur <khalifahnur1095@gmail.com>
2025-03-04 16:00:22 +0300

    added elpers functions

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -28,3 +28,44 @@ export const goToSettings = () => {
     Linking.openSettings();
   }
 };
+
+export const formatCurrency = (amount) => {
+  return `Ksh.${amount?.toFixed(2)}`;
+};
+
+export const calculateTotal = (menuItems) => {
+  return menuItems?.reduce((total, item) => {
+    return (
+      total +
+      parseInt(item.cost) * parseInt(item.quantity)
+    );
+  }, 0);
+};
+
+export const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
+
+export const getStatusColor = (status) => {
+    switch (status) {
+      case "Served":
+        return "#4CAF50"; // Green
+      case "Not-served":
+        return "#FF9800"; // Orange
+      case "Cancelled":
+        return "#F44336"; // Red
+      default:
+        return "#2196F3"; // Blue
+    }
+  };
+
+  export   const getPaymentColor = (paid) => {
+    return paid === "Paid" ? "#2196F3" : "#FF5722";
+  };
